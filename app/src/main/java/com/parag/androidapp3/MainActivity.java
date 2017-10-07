3e05bda6e7233f1d2c2045161dbafb2f44fccbe3
Paragniist <paraniist@gmail.com>
2017-10-08 00:17:10 +0530

    Initial commit

---
+++ app/src/main/java/com/parag/androidapp3/MainActivity.java
@@ -0,0 +1,27 @@
+package com.parag.androidapp3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b1 = (Button)findViewById(R.id.btn1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText et1 = (EditText) findViewById(R.id.et1);
+                TextView tv1 = (TextView) findViewById(R.id.tv1);
+                tv1.setText(et1.getText().toString());
+            }
+        });
+    }
+}
